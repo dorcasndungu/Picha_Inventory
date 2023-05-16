281cdcc540ff1a03a670e33fd3b8f9676b856c8c
dorcasndungu <ndungumwihaki017@gmail.com>
2023-05-16 09:19:33 +0300

    initial commit

---
+++ app/src/main/java/com/example/pichainventory/ui/SalesActivity.java
@@ -0,0 +1,16 @@
+package com.example.pichainventory.ui;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.pichainventory.R;
+
+public class SalesActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_sales);
+    }
+}
